d5a8a65b92204284876dd2a2236b666ee2f0ab4b
Vinayak Bagaria <workvinayak95@gmail.com>
2021-06-12 20:03:57 +0530

    Connect implementation

--- redux/basic.js
+++ redux/basic.js
@@ -1,16 +1,27 @@
 function createStore(reducer, initialState) {
   let state = initialState;
+  let listeners = [];
 
   function getState() {
     return state;
   }
 
+  function subscribe(listener) {
+    listeners.push(listener);
+    return function unsubscribe() {
+      const index = listeners.indexOf(listener);
+      listeners.splice(index, 1);
+    };
+  }
+
   function dispatch(action) {
     state = reducer(state, action);
+    listeners.forEach((listener) => listener());
   }
 
   return {
     getState,
+    subscribe,
     dispatch,
   };
 }
@@ -30,10 +41,13 @@ function rootReducer(state, action) {
 
 const store = createStore(rootReducer, initialState);
 
+store.subscribe(() => {
+  console.log(store.getState());
+});
+
 for (const element of Array(10)) {
   store.dispatch({
     type: 'add',
     by: 1,
   });
-  console.log(store.getState());
 }
---
+++ redux/connect.js
@@ -0,0 +1,27 @@
+function connect(mapStateToProps, mapDispatchToProps) {
+  return function (WrappedComponent) {
+    return class ConnectedWrapper extends React.Component {
+      componentDidMount() {
+        this.unsubscribe = store.subscribe(this.handleChange);
+      }
+
+      componentWillUnmount() {
+        this.unsubscribe();
+      }
+
+      handleChange = () => {
+        this.forceUpdate();
+      };
+
+      render() {
+        return (
+          <WrappedComponent
+            {...this.props}
+            {...mapStateToProps(store.getState(), this.props)}
+            {...WrappedComponent(store.getState(), this.props)}
+          />
+        );
+      }
+    };
+  };
+}
