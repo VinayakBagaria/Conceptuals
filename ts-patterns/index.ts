a08e6f98502e2748e578fe9e347af447f4de7c6b
Vinayak Bagaria <workvinayak95@gmail.com>
2021-06-13 15:57:52 +0530

    Observer pattern

--- ts-patterns/index.ts
+++ ts-patterns/index.ts
@@ -1,3 +1,33 @@
+// Observer
+type Listener<EventType> = (event: EventType) => void;
+function createObserver<EventType>(): {
+  subscribe: (listener: Listener<EventType>) => () => void;
+  publish: (event: EventType) => void;
+} {
+  let listeners: Listener<EventType>[] = [];
+
+  return {
+    subscribe: (listener: Listener<EventType>): (() => void) => {
+      listeners.push(listener);
+      return () => {
+        listeners = listeners.filter((l) => l !== listener);
+      };
+    },
+    publish: (event: EventType) => {
+      listeners.forEach((l) => l(event));
+    },
+  };
+}
+
+interface BeforeSetEvent<T> {
+  value: T;
+  newValue: T;
+}
+
+interface AfterSetEvent<T> {
+  value: T;
+}
+
 interface BaseRecord {
   id: string;
 }
@@ -5,17 +35,49 @@ interface BaseRecord {
 interface Database<T extends BaseRecord> {
   set(newValue: T): void;
   get(id: string): T | undefined;
+
+  onBeforeAdd(listener: Listener<BeforeSetEvent<T>>): () => void;
+  onAfterAdd(listener: Listener<AfterSetEvent<T>>): () => void;
 }
 
-class InMemoryDatabase<T extends BaseRecord> implements Database<T> {
-  private db: Record<string, T> = {};
+// Factory pattern
+function createDatabase<T extends BaseRecord>() {
+  class InMemoryDatabase implements Database<T> {
+    private db: Record<string, T> = {};
 
-  public set(newValue: T): void {
-    this.db[newValue.id] = newValue;
+    // Singleton pattern
+    static instance: InMemoryDatabase = new InMemoryDatabase();
+
+    private beforeAddListeners = createObserver<BeforeSetEvent<T>>();
+    private afterAddListeners = createObserver<AfterSetEvent<T>>();
+
+    private constructor() {}
+
+    public set(newValue: T): void {
+      this.beforeAddListeners.publish({
+        newValue,
+        value: this.db[newValue.id],
+      });
+      this.db[newValue.id] = newValue;
+      this.afterAddListeners.publish({
+        value: newValue,
+      });
+    }
+
+    public get(id: string): T | undefined {
+      return this.db[id];
+    }
+
+    onBeforeAdd(listener: Listener<BeforeSetEvent<T>>): () => void {
+      return this.beforeAddListeners.subscribe(listener);
+    }
+
+    onAfterAdd(listener: Listener<AfterSetEvent<T>>): () => void {
+      return this.afterAddListeners.subscribe(listener);
+    }
   }
-  get(id: string): T | undefined {
-    return this.db[id];
-  }
+
+  return InMemoryDatabase;
 }
 
 // usage
@@ -25,10 +87,22 @@ interface Pokemon {
   defense: number;
 }
 
-const pokemonDb = new InMemoryDatabase<Pokemon>();
-pokemonDb.set({
+const PokemonDb = createDatabase<Pokemon>();
+
+const unsubscribe = PokemonDb.instance.onAfterAdd(({ value }) => {
+  console.log(value);
+});
+
+PokemonDb.instance.set({
   id: 'Bulbasaur',
   attack: 50,
   defense: 10,
 });
-console.log(pokemonDb.get('Bulbasaur'));
+
+unsubscribe();
+
+PokemonDb.instance.set({
+  id: 'Spinosaur',
+  attack: 100,
+  defense: 20,
+});
