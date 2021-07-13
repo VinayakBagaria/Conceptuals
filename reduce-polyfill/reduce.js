50baac5f113207b85587a164bba920ff6a529502
Vinayak Bagaria <workvinayak95@gmail.com>
2021-07-14 03:39:49 +0530

    Reduce implemented with tests

--- reduce-polyfill/reduce.js
+++ reduce-polyfill/reduce.js
@@ -1,5 +1,36 @@
-function reduce() {
-  throw new TypeError('Array.prototype.reduce called on null or undefined');
+function reduce(callback, initialValue) {
+  if (this === null || this === undefined) {
+    throw new TypeError('Array.prototype.reduce called on null or undefined');
+  }
+
+  if (!callback || typeof callback !== 'function') {
+    throw new TypeError(`${callback} is not a function`);
+  }
+
+  if (!this.length) {
+    if (arguments.length < 2) {
+      throw new TypeError('Reduce of empty array with no initial value');
+    } else if (arguments.length === 2) {
+      return initialValue;
+    }
+  }
+
+  var k = 0;
+  var acc = initialValue;
+
+  if (arguments.length < 2) {
+    acc = this[k];
+    k++;
+  }
+
+  while (k < this.length) {
+    if (Object.prototype.hasOwnProperty.call(this, k)) {
+      acc = callback(acc, this[k], k, this);
+      k++;
+    }
+  }
+
+  return acc;
 }
 
 module.exports = reduce;
--- reduce-polyfill/reduce.test.js
+++ reduce-polyfill/reduce.test.js
@@ -2,15 +2,66 @@ const reduce = require('./reduce');
 
 const sum = (s, v) => s + v;
 
-describe('Error conditions', () => {
+describe('Error Conditions', () => {
   beforeAll(() => {
     Array.prototype.customReduce = reduce;
   });
 
-  test('Check if this exists, reduce is not called on null or undefined', () => {
-    function init() {
-      Array.prototype.customReduce.call(null, sum, 2);
+  // test('Check if this exists, reduce is not called on null or undefined', () => {
+  //   function init() {
+  //     Array.prototype.customReduce.call(null, sum, 2);
+  //   }
+  //   expect(init).toThrow(TypeError);
+  // });
+
+  test('Check if callback is a function', () => {
+    expect(() => [].customReduce()).toThrow(TypeError);
+  });
+
+  test('Reduce on empty array with no initialValue', () => {
+    expect(() => [].customReduce(sum)).toThrow(TypeError);
+  });
+});
+
+describe('Reduce Functionality', () => {
+  beforeAll(() => {
+    Array.prototype.customReduce = reduce;
+  });
+
+  test('Invoked on empty array with initialValue, returns initialValue', () => {
+    const initialValue = 5;
+    expect([].customReduce(sum, initialValue)).toBe(initialValue);
+  });
+
+  test('Invoked with initial Value', () => {
+    expect([1, 2, 3].customReduce(sum, 1)).toEqual(7);
+  });
+
+  test('Invoked without initial Value', () => {
+    expect([1, 2, 3].customReduce(sum)).toEqual(6);
+  });
+
+  test('Custom Reduce, with Promises', () => {
+    function first() {
+      return Promise.resolve(1);
     }
-    expect(init).toThrow(TypeError);
+
+    function second(v) {
+      return Promise.resolve(v + 2);
+    }
+
+    function third(v) {
+      return Promise.resolve(v + 3);
+    }
+
+    function fourth(v) {
+      return Promise.resolve(v + 4);
+    }
+
+    const promises = [first, second, third, fourth];
+    const answer = promises.customReduce((acc, current) => {
+      return acc.then(current);
+    }, Promise.resolve());
+    expect(answer).resolves.toEqual(10);
   });
 });
